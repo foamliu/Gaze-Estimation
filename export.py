e48e14fea1a54d2bb9885f197fb3505f701afcf0
Yang Liu <liuyang12@focusmedia.cn>
2019-12-12 17:01:44 +0800

    update

--- export.py
+++ export.py
@@ -2,7 +2,7 @@ import time
 
 import torch
 
-from models import ResNetMatchModel
+from models import GazeEstimationModel
 
 if __name__ == '__main__':
     checkpoint = 'BEST_checkpoint.tar'
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     model = checkpoint['model'].module
     metric_fc = checkpoint['metric_fc'].module
 
-    filename = 'image_matching.pt'
+    filename = 'gaze_estimation.pt'
     print('saving {}...'.format(filename))
     start = time.time()
     torch.save(model.state_dict(), filename)
@@ -21,11 +21,11 @@ if __name__ == '__main__':
 
     print('loading {}...'.format(filename))
     start = time.time()
-    model = ResNetMatchModel()
+    model = GazeEstimationModel()
     model.load_state_dict(torch.load(filename))
     print('elapsed {} sec'.format(time.time() - start))
 
-    filename_scripted = 'image_matching_scripted.pt'
+    filename_scripted = 'gaze_estimation_scripted.pt'
     print('saving {}...'.format(filename_scripted))
     start = time.time()
     torch.jit.save(torch.jit.script(model), filename_scripted)
