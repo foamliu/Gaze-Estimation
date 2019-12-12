fb050eb4cff1fb9f5bea65334244f55257954bd7
Yang Liu <liuyang12@focusmedia.cn>
2019-12-12 15:43:14 +0800

    update

--- rotate_cube.py
+++ rotate_cube.py
@@ -24,8 +24,8 @@ def main():
 
     for i in range(10):
         item = data[i]
-        pitch = item['out'][0]
-        roll = item['out'][1]
+        pitch = item['out'][1]
+        roll = item['out'][0]
         yaw = -item['out'][2]
         draw(1, yaw, pitch, roll)
         pygame.image.save(screen, "images/{}_angle.jpg".format(i))
