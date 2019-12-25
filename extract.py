2f34e3ef98943d28e5cbce87c33613d5901db81b
Yang Liu <liuyang12@focusmedia.cn>
2019-12-25 17:59:25 +0800

    update

--- extract.py
+++ extract.py
@@ -1,5 +1,14 @@
+import tarfile
+
 from utils import extract
+
+
+def extract_tar(filename):
+    print('Extracting {}...'.format(filename))
+    with tarfile.open(filename) as tar:
+        tar.extractall('data')
+
 
 if __name__ == "__main__":
     extract('data/imgs.zip')
-
+    extract_tar('data/MPIIGaze.tar.gz')
